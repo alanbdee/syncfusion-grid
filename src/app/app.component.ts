84560f4c1a300ead143c29f097a6ababee612950
Alan Dee <alandee@savageservices.com>
2019-08-09 10:27:36 -0600

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {data} from './datasource';
 import {PageSettingsModel} from '@syncfusion/ej2-grids';
+import {SortService} from '@syncfusion/ej2-angular-grids';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [SortService]
 })
 export class AppComponent implements OnInit {
   title = 'syncfusion-grid';
